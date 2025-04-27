5161e9d96e4a2f31f43aff87b006f2b4cf9d01ab
Nguyễn Ngọc Tuân <89716508+tuanotuan@users.noreply.github.com>
2025-04-27 10:25:54 +0700

    Time: 492 ms (7.71%) | Memory: 229.9 MB (86.96%) - LeetSync

--- 3729-unit-conversion-i/unit-conversion-i.cpp
+++ 3729-unit-conversion-i/unit-conversion-i.cpp
@@ -1,37 +1,29 @@
-static const int MOD = 1000000007;
-
+const int MOD = 1000000007;
 class Solution {
 public:
     vector<int> baseUnitConversions(vector<vector<int>>& conversions) {
         int n = (int)conversions.size() + 1;
-        // 1) Xây đồ thị có hướng (cục bộ, không cần clear lại)
-        vector<vector<pair<int,int>>> adj(n);
+        vector < long long > ans(n);
+        vector < pair < int, int > > adj[100005];
         for (auto &e : conversions) {
             int u = e[0], v = e[1], w = e[2];
             adj[u].emplace_back(v, w);
         }
-
-        // 2) BFS từ đỉnh 0
-        vector<long long> ans(n, 0);
-        queue<int> q;
+        ans.resize(n,0);
         ans[0] = 1;
+        queue < int > q;
         q.push(0);
-
-        while (!q.empty()) {
-            int u = q.front(); q.pop();
-            for (auto &edge : adj[u]) {
-                int v = edge.first;
-                int w = edge.second;
-                ans[v] = (ans[u] * w) % MOD;
-                q.push(v);
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(auto [v,w]: adj[u]){
+            ans[v]  = (ans[u] * w) % MOD;
+            q.push(v);
             }
         }
-
-        // 3) Chuyển sang vector<int> kết quả
-        vector<int> res(n);
-        for (int i = 0; i < n; i++) {
-            res[i] = (int)ans[i];
-        }
+        vector < int > res(n);
+        for(int i = 0; i < n; i++)
+        res[i] = ans[i];
         return res;
     }
 };
