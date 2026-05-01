244cc5a7dd3eaad97ec451bf78ef19fd83c39dff
Nguyễn Ngọc Tuân <89716508+tuanotuan@users.noreply.github.com>
2026-05-01 16:00:47 +0700

    Time: 40 ms (11.04%) | Memory: 156.3 MB (6.15%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        long long totalSum = 0;
-
-        // Bước 1: Tính tổng toàn bộ grid
-        // Dùng long long để tránh tràn số (overflow)
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                totalSum += grid[i][j];
+        int n = grid.size();
+        int m = grid[0].size();
+        vector < vector < long long > > sum(n, vector < long long > (m,0));
+        for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++){
+            if(i == 0 && j == 0){
+                sum[i][j] = grid[i][j];
+                continue;
             }
+            else if(i == 0)
+            sum[i][j] = sum[i][j-1] + grid[i][j];
+            else if(j == 0)
+            sum[i][j] = sum[i-1][j] + grid[i][j];
+            else sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + grid[i][j];
         }
-
-        // Nếu tổng lẻ thì không bao giờ chia đôi bằng nhau được
-        if (totalSum % 2 != 0) return false;
-        long long target = totalSum / 2;
-
-        // Bước 2: Kiểm tra cắt ngang (Horizontal cut)
-        // Duyệt qua từng hàng, cộng dồn tổng hàng vào rowSum
-        long long currentRowSum = 0;
-        for (int i = 0; i < m - 1; ++i) { // m-1 vì mỗi phần phải non-empty
-            for (int j = 0; j < n; ++j) {
-                currentRowSum += grid[i][j];
-            }
-            if (currentRowSum == target) return true;
-        }
-
-        // Bước 3: Kiểm tra cắt dọc (Vertical cut)
-        // Duyệt qua từng cột, cộng dồn tổng cột vào colSum
-        long long currentColSum = 0;
-        for (int j = 0; j < n - 1; ++j) { // n-1 vì mỗi phần phải non-empty
-            for (int i = 0; i < m; ++i) {
-                currentColSum += grid[i][j];
-            }
-            if (currentColSum == target) return true;
-        }
-
+        for(int i = 0; i < n - 1; i++)
+        if(2 * sum[i][m-1] == sum[n-1][m-1]) return true;
+        for(int j = 0; j < m - 1; j++)
+        if(2 * sum[n-1][j] == sum[n-1][m-1]) return true;
         return false;
     }
 };
